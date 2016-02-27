13a1aa2fe3d523d25e72bf1e38f0b06b328bbaba
Lewis Pollard <lewiji@gmail.com>
2016-02-27 23:02:14 +0000

    Navmesh undo

--- demo/src/game.js
+++ demo/src/game.js
@@ -8,7 +8,8 @@ pncgame.prototype = {
 	},
 	create: function () {
 		var sceneDefinition = {
-			bg: 'img/testbg.png'
+			bg: 'img/testbg.png',
+			pathPolys: [{"area":-27737,"_points":[{"x":166,"y":229,"type":25},{"x":187,"y":249,"type":25},{"x":249,"y":222,"type":25},{"x":319,"y":238,"type":25},{"x":319,"y":262,"type":25},{"x":305,"y":283,"type":25},{"x":340,"y":298,"type":25},{"x":387,"y":304,"type":25},{"x":389,"y":323,"type":25},{"x":430,"y":333,"type":25},{"x":450,"y":318,"type":25},{"x":486,"y":313,"type":25},{"x":469,"y":289,"type":25},{"x":498,"y":263,"type":25},{"x":518,"y":245,"type":25},{"x":476,"y":237,"type":25},{"x":470,"y":205,"type":25},{"x":464,"y":183,"type":25},{"x":462,"y":176,"type":25},{"x":412,"y":164,"type":25},{"x":394,"y":164,"type":25},{"x":380,"y":176,"type":25},{"x":354,"y":193,"type":25},{"x":338,"y":180,"type":25},{"x":328,"y":171,"type":25},{"x":302,"y":180,"type":25},{"x":275,"y":185,"type":25}],"closed":true,"type":12}]
 		};
 		// creates a scene and immediately switches to it
 		var room = this.game.pncPlugin.addScene('lobby', sceneDefinition, true);
--- src/DebugNavmesh.js
+++ src/DebugNavmesh.js
@@ -1,6 +1,5 @@
 Phaser.Plugin.PNCAdventure.DebugNavmesh = function (game) {
 	this.game = game;
-	this.initSignals();
 	this.initPoints();
 	this.initGraphics();
 	this.elOutput = document.getElementById('navmeshDebug');
@@ -12,6 +11,8 @@ Phaser.Plugin.PNCAdventure.DebugNavmesh = function (game) {
 		this.elOutput.style.display = 'none';
 		document.body.appendChild(this.elOutput);
 	}
+	this.toggleKey = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
+	this.toggleKey.onDown.add(Phaser.Plugin.PNCAdventure.DebugNavmesh.prototype.toggleTool, this);
 };
 
 Phaser.Plugin.PNCAdventure.DebugNavmesh.prototype = {
@@ -25,20 +26,42 @@ Phaser.Plugin.PNCAdventure.DebugNavmesh.prototype = {
 		this.confirmKey = this.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
 		this.confirmKey.onDown.add(Phaser.Plugin.PNCAdventure.DebugNavmesh.prototype.createPoly, this);
 
-		this.toggleKey = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
-		this.toggleKey.onDown.add(Phaser.Plugin.PNCAdventure.DebugNavmesh.prototype.toggleTool, this);
+		this.undoKey = this.game.input.keyboard.addKey(Phaser.Keyboard.Z);
+		this.undoKey.onDown.add(Phaser.Plugin.PNCAdventure.DebugNavmesh.prototype.undo, this);		
+	},
+	removeSignals: function () {
+		this.confirmKey.onDown.remove(Phaser.Plugin.PNCAdventure.DebugNavmesh.prototype.createPoly, this);
+		this.undoKey.onDown.remove(Phaser.Plugin.PNCAdventure.DebugNavmesh.prototype.undo, this);
+
+		this.game.pncPlugin.signals.sceneTappedSignal.remove(
+			Phaser.Plugin.PNCAdventure.DebugNavmesh.prototype.drawPoint,
+			this, 5000
+		);
 	},
 	toggleTool: function () {
 		if (this.enabled) {
+			this.removeSignals();
+			Phaser.Plugin.PNCAdventure.debugMode = false;
 			this.enabled = false;
 			this.graphics.visible = false;
-			this.elOutput.style.display = 'none';
 		} else {
+			this.initSignals();
+			Phaser.Plugin.PNCAdventure.debugMode = true;
 			this.enabled = true;
 			this.graphics.visible = true;
 			this.elOutput.style.display = 'block';
 		}
 		
+	},
+	undo: function () {
+		if (this.currentPoints.length > 0) {
+			this.currentPoints.pop();
+		} else {
+			this.finishedPolys.pop();
+		}
+		this.graphics.clear();
+		this.drawFinishedPolys();
+		this.drawCurrentPoints();
 	},
 	initPoints: function () {
 		this.currentPoints = [];
@@ -48,25 +71,26 @@ Phaser.Plugin.PNCAdventure.DebugNavmesh.prototype = {
 		this.graphics = this.game.add.graphics(0, 0);
 	},
 	drawPoint: function (e) {
-		if (!this.enabled) {
-			return;
-		}
 		// prevent onInputUp signal propogation
 		this.game.pncPlugin.signals.sceneTappedSignal.halt();
 		console.debug('Draw point');
-		this.graphics.beginFill(0xFF00FF);
-		this.graphics.drawCircle(e.x, e.y, 10);
-		this.graphics.endFill();
 		this.currentPoints.push({x: e.x, y: e.y});
+		this.drawFinishedPolys();
+		this.drawCurrentPoints();
+	},
+	drawCurrentPoints: function () {
+		for (var i = 0; i < this.currentPoints.length; i++) {
+			this.graphics.beginFill(0xFF00FF);
+			this.graphics.drawCircle(this.currentPoints[i].x, this.currentPoints[i].y, 10);
+			this.graphics.endFill();
+		}
+		
 	},
 	clearPoints: function () {
 		this.graphics.clear();
 		this.currentPoints = [];
 	},
 	createPoly: function () {
-		if (!this.enabled) {
-			return;
-		}
 		if (this.currentPoints.length < 3) {
 			console.warn('Not enough points to draw poly');
 			return;
